5c6a5a0401ce7d14017b90e919726b174c26cb44
JacobWylie <jacobwylie@gmail.com>
2017-02-15 13:27:18 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// problem: hints are shown when form is valid
+// solution: hide and show at appropriate times
+
+// hide hints
+
+// when event happens on password input
+	// find out if password is valid
+		// hide hint if valid
+		// else show hint
+
+// when event happens on confirmation input
+	// find out if password and confirmation match
+		// hide hint if match
+		// else show hint
