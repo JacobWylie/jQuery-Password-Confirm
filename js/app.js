d30f3918f24517cc51f28fee743a1f35132044d1
JacobWylie <jacobwylie@gmail.com>
2017-02-15 14:45:15 +0100

    Disabled submit button until password parameters met

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@ var $confirmPassword = $("#confirm_password");
 // hide hints
 $("form span").hide();
 
+function isPasswordValid() {
+	return $password.val().length > 8;
+}
+
+function arePasswordsMatching() {
+	return $password.val() === $confirmPassword.val();
+}
+
+function canSubmit() {
+	return isPasswordValid() && arePasswordsMatching();
+}
+
 function passwordEvent(){
 	// find out if password is valid
-	if($password.val().length > 8) {
+	if(isPasswordValid()) {
 		// hide hint if valid
 		$password.next().hide();
 	} else {
@@ -18,7 +30,7 @@ function passwordEvent(){
 
 function confirmPasswordEvent() {
 	// find out if password and confirmation match
-	if ($password.val() === $confirmPassword.val()) {
+	if(arePasswordsMatching()) {
 		// hide hint if match
 		$confirmPassword.next().hide();		
 	} else {
@@ -27,9 +39,36 @@ function confirmPasswordEvent() {
 	}
 }
 
+function enableSubmitEvent() {
+	$("#submit").prop("disabled", !canSubmit());
+}
+
 // when event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
 
 
 // when event happens on confirmation input
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+enableSubmitEvent();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
